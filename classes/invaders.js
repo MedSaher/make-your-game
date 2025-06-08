8e22c0ed937b4cdf12d4f94092d1df3dd442542c
samohamed <mohamed.saher.23@ump.ac.ma>
2025-06-08 13:37:05 +0100

    Work in progress: update invaders.js

--- classes/invaders.js
+++ classes/invaders.js
@@ -1,4 +1,5 @@
 import { Field } from "./field.js";
+import { Bullet } from "./bullet.js";
 
 export class Invader extends Field {
     toggleType() {
@@ -8,5 +9,8 @@ export class Invader extends Field {
             this.element.classList.remove("type1", "type2");
             this.element.classList.add(this.type);
         }
+    }
+    pullTheTrigger(){
+        const bullet = new Bullet(1.2, 2.5, "invader_bullet", battlefield, null, "sprites/Projectiles/ProjectileC_4.png");
     }
 }
